d1894120b001d046e75654a009acd5faa8339b76
fathan <fathanhasfi@gmail.com>
2024-03-07 14:45:37 +0700

    merevisi commit

--- fungsi.js
+++ fungsi.js
@@ -6,8 +6,8 @@ console.log(add(1, 2));
 const substract = (a, b) => a - b
 console.log(substract(5, 2))
 
-const multiply = (a, b) => a * b
-console.log(multiply(2, 2))
+const multiplyNumber = (a, b) => a * b
+console.log(multiplyNumber(2, 2))
 
 const divide = (a, b) => a / b
 console.log(divide(4, 2))
