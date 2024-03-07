eac17d374b362ef1b9640002494db0c52664b11e
fathan <fathanhasfi@gmail.com>
2024-03-07 14:37:54 +0700

    ngulang data

--- fungsi.js
+++ fungsi.js
@@ -5,3 +5,9 @@ console.log(add(1, 2));
 
 const substract = (a, b) => a - b
 console.log(substract(5, 2))
+
+const multiply = (a, b) => a * b
+console.log(multiply(2, 2))
+
+const divide = (a, b) => a / b
+console.log(divide(4, 2))
