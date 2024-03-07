1acd32d9b87fa028f6871958ed21150f2a0d3664
fathan <fathanhasfi@gmail.com>
2024-03-07 14:26:52 +0700

    Revert "gak jelas"
    This reverts commit b7026647dcb7b574f7d223cf4bf4bad3b71693b9.

--- fungsi.js
+++ fungsi.js
@@ -3,12 +3,8 @@ console.log("anak haram");
 const add = (a, b) => a + b;
 console.log(add(1, 2));
 
-const substract = (a, b) => a - b;
-console.log(substract(5, 2));
+const substract = (a, b) => a - b
+console.log(substract(5, 2))
 
 const multiply = (a, b) => a * b;
-console.log(multiply(2, 2));
-
-
-const divide = (a, b) => a / b
-console.log(divide(4, 2))
+console.log(multiply(2, 2))
