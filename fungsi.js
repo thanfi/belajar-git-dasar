3de32cb27270d8a6a617874408bf78dc345018ed
fathan <fathanhasfi@gmail.com>
2024-03-07 14:14:11 +0700

    menambahkan fungsi pembagian

--- fungsi.js
+++ fungsi.js
@@ -1,10 +1,14 @@
-console.log('anak haram');
+console.log("anak haram");
 
 const add = (a, b) => a + b;
 console.log(add(1, 2));
 
-const substract = (a, b) => a - b
-console.log(substract(5, 2))
+const substract = (a, b) => a - b;
+console.log(substract(5, 2));
 
 const multiply = (a, b) => a * b;
-console.log(multiply(2, 2))
+console.log(multiply(2, 2));
+
+
+const divide = (a, b) => a / b
+console.log(divide(4, 2))
